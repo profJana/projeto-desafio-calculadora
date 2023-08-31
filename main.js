338708a98420d722a38b759d89e22bc3994d2b1a
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-08-31 19:05:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function somar() {
+    var v1 = parseInt(); //parseInt transforma em número inteiro o valor 
+    var v2 = parseInt();
+    
+    if (isNaN(v1) || isNaN(v2)) { //se algum dos campos estiver vazio:
+        alert("Campo não informado: preencha um valor");
+
+    } else if(v1 < 0 || v2 < 0){ //se alguns dos números é negativo
+        alert("Não pode conter número negativo")
+        
+    } else {
+        
+    }
+
+}
+
